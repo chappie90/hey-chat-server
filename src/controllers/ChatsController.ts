94cfe9b35385d2f8ef5c065bd4795b55b5af9fbe
Stoyan <stoyan.garov@yahoo.com>
2020-12-15 23:11:12 +0000

    Mark unmuted chats

--- src/controllers/ChatsController.ts
+++ src/controllers/ChatsController.ts
@@ -43,10 +43,7 @@ const getChats = async (req: Request, res: Response, next: NextFunction): Promis
       chat.unreadMessagesCount = unreadMessagesCount;
 
       // Get muted chats
-      if (user.mutedChats.includes(chat._id)) {
-        console.log(chat._id);
-        chat.muted = true;
-      }
+      chat.muted = user.mutedChats.includes(chat._id) ? true : false;
     } 
 
     res.status(200).send({ chats });
