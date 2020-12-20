c6ff1e2038cf1affb54c7e5a4ff1602fc5191b87
Stoyan <stoyan.garov@yahoo.com>
2020-12-20 23:35:31 +0000

    Troubleshoot push notifications android

--- src/helpers/sendPushNotification.ts
+++ src/helpers/sendPushNotification.ts
@@ -36,9 +36,9 @@ const sendPushNotification = async (
           "sound":"default",
           // "icon":
         },
-        "data":{
-          "silent":"false"
-        },
+        // "data":{
+        //   "silent":"false"
+        // },
         "priority":"high",
       },
       token:deviceToken
