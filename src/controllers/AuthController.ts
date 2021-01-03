c8ba30454867c8d8ec9821e6242e49f1eb6dc472
Stoyan <stoyan.garov@yahoo.com>
2021-01-03 00:47:30 +0000

    Merge profile and auth routes

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 import jwt from 'jsonwebtoken';
 const User = mongoose.model('User');
 
+import convertImage from '../helpers/convertImage';
+import resizeImage from '../helpers/resizeImage';
+import uploadFileS3 from '../helpers/uploadFileS3';
+import deleteFileS3 from '../helpers/deleteFileS3';
+import { transformImageName } from '../middleware/processUploads';
+
+const PROFILE_IMG_FOLDER = 'public/uploads/profile';
+
 const signup = async (req: Request, res: Response, next: NextFunction): Promise<any | void> => {
   const { username, password } = req.body;
 
@@ -51,7 +59,108 @@ const signin = async (req: Request, res: Response, next: NextFunction): Promise<
   }
 };
 
+const getAvatarImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { userId } = req.query;
+
+    const user = await User.findOne({ _id: userId });
+
+    res.status(200).send({ profileImage: user.avatar.medium });
+  } catch(err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const uploadAvatarImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try { 
+    const imageFile = req.file;
+    let bufferOriginal = req.file.buffer;
+    const mimeType = req.file.mimetype;
+    const imageNameOriginal = transformImageName(imageFile);
+    const imageNameSmall = transformImageName(imageFile, 'small');
+    const imageNameMedium = transformImageName(imageFile, 'medium');
+    const userId = req.body.userId;
+
+    let oldImageNameOriginal: string,
+        oldImageNameSmall: string,
+        oldImageNameMedium: string;
+
+    // Convert heic / heif images to jpg because jimp doesn't support format
+    // Returns converted image buffer
+    if (mimeType === 'image/heic') {
+      bufferOriginal = await convertImage(bufferOriginal);
+    }
+
+    // Create different size versions of original image 
+    // Returns buffer output
+    const bufferSmall = await resizeImage(bufferOriginal, mimeType, 'small', next);
+    const bufferMedium = await resizeImage(bufferOriginal, mimeType, 'medium', next);
+
+    // Get reference to old profile images to delete later
+    const user = await User.findOne({ _id: userId });
+    if (user.avatar.original) {
+      oldImageNameOriginal = user.avatar.original;
+      oldImageNameMedium = user.avatar.medium;
+      oldImageNameSmall = user.avatar.small;
+    }
+
+    // Upload TO AWS S3 bucket
+    // Returns bucket image path
+    await uploadFileS3(bufferOriginal, imageNameOriginal, mimeType, `${PROFILE_IMG_FOLDER}/original`, next);
+    await uploadFileS3(bufferSmall, imageNameSmall, mimeType,`${PROFILE_IMG_FOLDER}/small`, next);
+    await uploadFileS3(bufferMedium, imageNameMedium, mimeType, `${PROFILE_IMG_FOLDER}/medium`, next);
+
+    await User.updateOne(
+      { _id: userId },
+      { avatar: {
+        original: imageNameOriginal,
+        small: imageNameSmall,
+        medium: imageNameMedium,
+      } }
+    );
+
+    res.status(200).send({ profileImage: imageNameMedium }); 
+
+    // Delete old profile images
+    if (oldImageNameOriginal) {
+      deleteFileS3(oldImageNameOriginal, `${PROFILE_IMG_FOLDER}/original`);
+      deleteFileS3(oldImageNameSmall, `${PROFILE_IMG_FOLDER}/small`);
+      deleteFileS3(oldImageNameMedium, `${PROFILE_IMG_FOLDER}/medium`);
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const deleteAvatarImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { userId } = req.body;
+
+  try {
+    const user = await User.findOne({ _id: userId });
+    if (user.avatar.original) {
+      await deleteFileS3(user.avatar.original, `${PROFILE_IMG_FOLDER}/original`);
+      await deleteFileS3(user.avatar.medium, `${PROFILE_IMG_FOLDER}/medium`);
+      await deleteFileS3(user.avatar.small, `${PROFILE_IMG_FOLDER}/small`);
+    }
+    
+    await User.updateOne(
+      { _id: userId },
+      { avatar: { original: '', small: '', medium: '' } }
+    );
+
+    res.status(200).send({ imageDeleted: true });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
 export default {
   signup,
-  signin
+  signin,
+  getAvatarImage,
+  uploadAvatarImage,
+  deleteAvatarImage
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import AuthCtrl from '../controllers/AuthController';
+import { memoryStorage } from '../middleware/processUploads';
 
 const router = Router();
 
 router.post('/api/signup', AuthCtrl.signup);
 router.post('/api/signin', AuthCtrl.signin);
+router.get('/api/image', AuthCtrl.getAvatarImage);
+router.post(
+  '/api/image/upload', 
+  multer({ storage: memoryStorage() }).single('profileImage'),
+  AuthCtrl.uploadAvatarImage
+);
+router.patch('/api/image/delete', AuthCtrl.deleteAvatarImage);
 
 export default router;
