eb0f16ab57eaed6d18bf35f94536dfe9a77069fd
Stoyan <stoyan.garov@yahoo.com>
2021-01-20 21:41:47 +0000

    Troubleshoot image magick buildpack

--- src/helpers/resizeImage.ts
+++ src/helpers/resizeImage.ts
@@ -18,31 +18,26 @@ const resizeImage = async (
     outputDimensions = [400, 400];
   }
 
-  gm(bufferInput, 'test.jpg')
-    .resize(outputDimensions[0], outputDimensions[1])
-    .toBuffer('jpeg',function(err, buffer) {
-      if (err) {
-        console.log(err);
-        next(err);
-        return;
-      } 
+  function gmToBuffer (data) {
+    return new Promise((resolve, reject) => {
+      data.toBuffer('jpeg', (err, stdout, stderr) => {
+        if (err) { return reject(err) }
+        const chunks = []
+        stdout.on('data', (chunk) => { chunks.push(chunk) });
+        // these are 'once' because they can and do fire multiple times for multiple errors,
+        // but this is a promise so you'll have to deal with them one at a time
+        stdout.once('end', () => { 
+          resolve(Buffer.concat(chunks));
+          console.log('success')
+        });
+        stderr.once('data', (data) => { reject(String(data)) })
+      })
+    })
+  }
 
-      console.log(buffer)
-    });
+  const data = gm(bufferInput, 'test.jpg').resize(outputDimensions[0], outputDimensions[1]);
 
-  // function gmToBuffer (data) {
-  //   return new Promise((resolve, reject) => {
-  //     data.toBuffer((err, stdout, stderr) => {
-  //       if (err) { return reject(err) }
-  //       const chunks = []
-  //       stdout.on('data', (chunk) => { chunks.push(chunk) })
-  //       // these are 'once' because they can and do fire multiple times for multiple errors,
-  //       // but this is a promise so you'll have to deal with them one at a time
-  //       stdout.once('end', () => { resolve(Buffer.concat(chunks)) })
-  //       stderr.once('data', (data) => { reject(String(data)) })
-  //     })
-  //   })
-  // }
+  gmToBuffer(data).then(console.log).catch(err => console.log(err));
 
   return null;
 };
