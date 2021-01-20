1d0ecfd221bf04baeed0d379f242f4b0e88fc533
Stoyan <stoyan.garov@yahoo.com>
2021-01-20 00:36:43 +0000

    Add image magick

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -98,35 +98,35 @@ const uploadAvatarImage = async (req: Request, res: Response, next: NextFunction
       console.log(req.file)
       console.log(bufferOriginal)
         
-        gm(bufferOriginal)
-          .resize(100, 100)
-          .toBuffer('jpeg',function (err, buffer) {
-            if (err) {
-              console.log(err);
-              return;
-            }
-            bufferTest = buffer;
-            console.log(bufferTest)
-            console.log('done!');
-        });
+        // gm(bufferOriginal)
+        //   .resize(100, 100)
+        //   .toBuffer('jpeg',function (err, buffer) {
+        //     if (err) {
+        //       console.log(err);
+        //       return;
+        //     }
+        //     bufferTest = buffer;
+        //     console.log(bufferTest)
+        //     console.log('done!');
+        // });
 
-        // function gmToBuffer (data) {
-        //   return new Promise((resolve, reject) => {
-        //     data.stream((err, stdout, stderr) => {
-        //       if (err) { return reject(err) }
-        //       const chunks = []
-        //       stdout.on('data', (chunk) => { chunks.push(chunk) })
-        //       // these are 'once' because they can and do fire multiple times for multiple errors,
-        //       // but this is a promise so you'll have to deal with them one at a time
-        //       stdout.once('end', () => { resolve(Buffer.concat(chunks)) })
-        //       stderr.once('data', (data) => { reject(String(data)) })
-        //     })
-        //   })
-        // }
+        function gmToBuffer (data) {
+          return new Promise((resolve, reject) => {
+            data.stream((err, stdout, stderr) => {
+              if (err) { return reject(err) }
+              const chunks = []
+              stdout.on('data', (chunk) => { chunks.push(chunk) })
+              // these are 'once' because they can and do fire multiple times for multiple errors,
+              // but this is a promise so you'll have to deal with them one at a time
+              stdout.once('end', () => { resolve(Buffer.concat(chunks)) })
+              stderr.once('data', (data) => { reject(String(data)) })
+            })
+          })
+        }
         
 
-        // const data = gm(bufferOriginal, 'output.jpg').resize(500)
-        // gmToBuffer(data).then(console.log)
+        const data = gm(bufferOriginal, 'output.jpg').resize(500)
+        gmToBuffer(data).then(console.log)
 
     }
 
