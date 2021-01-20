314636fcacd1e1bd8a010a6dec6650c053a92f40
Stoyan <stoyan.garov@yahoo.com>
2021-01-20 21:47:28 +0000

    Troubleshoot image magick buildpack

--- src/helpers/resizeImage.ts
+++ src/helpers/resizeImage.ts
@@ -20,17 +20,15 @@ const resizeImage = async (
 
   function gmToBuffer (data) {
     return new Promise((resolve, reject) => {
-      data.toBuffer('jpeg', (err, stdout, stderr) => {
-        if (err) { return reject(err) }
-        const chunks = []
-        stdout.on('data', (chunk) => { chunks.push(chunk) });
-        // these are 'once' because they can and do fire multiple times for multiple errors,
-        // but this is a promise so you'll have to deal with them one at a time
-        stdout.once('end', () => { 
-          resolve(Buffer.concat(chunks));
-          console.log('success')
-        });
-        stderr.once('data', (data) => { reject(String(data)) })
+      data.toBuffer('jpeg', (err, buffer) => {
+        if (err) { 
+          console.log(err);
+          return reject(err);
+        };
+        if (buffer) { 
+          console.log(buffer);
+          return resolve(buffer);
+        }
       })
     })
   }
