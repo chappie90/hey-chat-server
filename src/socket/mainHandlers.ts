414abc7cbf269c8d060a2566860281c970c5927e
Stoyan <stoyan.garov@yahoo.com>
2021-02-07 14:33:33 +0000

    Troubleshoot get contacts

--- src/socket/mainHandlers.ts
+++ src/socket/mainHandlers.ts
@@ -26,7 +26,7 @@ export const onConnect = async (
     ).lean()
       .populate('pendingContacts', 'username avatar.small')
       .populate('contacts', 'username avatar.small')
-      .populate('chats', 'chatId participants')
+      .populate('chats', 'chatId participants requester')
       .populate('deletedChats', 'participants');
 
     // Get user contacts
